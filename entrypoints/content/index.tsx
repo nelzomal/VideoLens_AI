2f6c5edac95fcefb0383d662596ecc754cbb984c
nelzomal <nelzomal@gmail.com>
2024-11-05 13:19:24 +0800

    feat: implement content UI

--- entrypoints/content/index.tsx
+++ entrypoints/content/index.tsx
@@ -1,7 +1,23 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
 export default defineContentScript({
   matches: ["<all_urls>"],
-  main() {
-    console.log("Hello content.");
-    // TODO show transcript
-  }
+  cssInjectionMode: "ui",
+  async main(ctx) {
+    const ui = await createShadowRootUi(ctx, {
+      name: "realtime-whisper-ui",
+      position: "inline",
+      onMount(container) {
+        console.log(container);
+        const root = ReactDOM.createRoot(container);
+        root.render(<App />);
+        return root;
+      },
+      onRemove: (root) => {
+        root?.unmount();
+      },
+    });
+    ui.mount();
+  },
 });
